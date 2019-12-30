7c5b0817b8ba11fa069227e04f2c72e763c0aff4
ProjectHSI <57279339+ProjectHSI@users.noreply.github.com>
2019-12-30 11:20:18 +1000

    Added Boot Into DOS Files,
    Changelog:
    The Following files were added:
    HSIROMDOS(Repository) -> BootintoCMD(Branch) -> HSIROM.bat
    HSIROMDOS(Repository) -> BootintoCMD(Branch) -> HSIROM -> HSIROMDOS -> HSIROMDOSMAIN.bat
    The following files were removed:
    (None)
    The following files were changed or moved:
    (None)

---
+++ HSIROM/HSIROM.bat
@@ -0,0 +1,28 @@
+ echo off
+pause
+cls
+echo ROM CHECK
+echo ---------
+echo OK
+echo ---------
+pause
+color 0E
+echo VIDEO CHK
+echo ---------
+echo OK
+echo ---------
+pause
+echo RAM CHECK
+echo ---------
+echo Waiting..
+echo ---------
+timeout 5
+echo gfuiuhih7noim7umfVU(J*JUVr87ju(yhbyhm#RT8YRHN*btnyinvg#@UKRFJYHNEujkubnryHGWBkuy$whjgbtrF7YIUkhbeyuhbrnGFUYwykh
+echo RAM CHECK
+echo ---------
+echo OK
+echo ---------
+pause
+echo Booted into DOS Mode, enter the following commands to continue into HSIROMDOS
+echo CD [WHERE EVER THE OS MIGHT BE INSTALLED]HSIROM\HSIROMDOS\HSIROMDOSMAIN
+cmd
